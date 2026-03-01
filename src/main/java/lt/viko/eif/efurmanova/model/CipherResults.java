40430455c65e750b2e0bb475d3d7781bf4c8700f
Evelina <evelina.furmanova@stud.viko.lt>
2026-03-01 14:32:18 +0200

    Vignere Cipher project start

--- src/main/java/lt/viko/eif/efurmanova/model/CipherResults.java
+++ src/main/java/lt/viko/eif/efurmanova/model/CipherResults.java
@@ -1,20 +1,20 @@
-package lt.viko.eif.efurmanova.model;
-
-public class CipherResults {
-
-    private final String text;
-    private final int keyIndex;
-
-    public CipherResults(String text, int keyIndex) {
-        this.text = text;
-        this.keyIndex = keyIndex;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public int getKeyIndex() {
-        return keyIndex;
-    }
-}
+//package lt.viko.eif.efurmanova.model;
+//
+//public class CipherResults {
+//
+//    private final String text;
+//    private final int keyIndex;
+//
+//    public CipherResults(String text, int keyIndex) {
+//        this.text = text;
+//        this.keyIndex = keyIndex;
+//    }
+//
+//    public String getText() {
+//        return text;
+//    }
+//
+//    public int getKeyIndex() {
+//        return keyIndex;
+//    }
+//}
